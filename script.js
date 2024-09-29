5897e1bfaa1d427319670f4ce625b485b6f654b3
AadiSKartha <adithyas.k4@gmail.com>
2024-09-29 18:19:10 +0530

    Added error shake animation

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 $(document).ready(function () {
-    let imgBox = $("#imgBox");
-    let qrImage = $("#qrImage");
-    let qrText = $("#qrText");
+    let imageBox = $("#imageBox")
+    let qrImage = $("#qrImage")
+    let inputText = $("#inputText")
 
-    window.getQrImage = function() {
-        if (qrText.val().trim() !== "") {
-            qrImage.attr("src", "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.val());
-            imgBox.addClass("show-img");
+    window.getQrImage = function () {
+        if (inputText.val().trim().length > 0) {
+            // API to generate and return QR Code image.
+            qrImage.attr("src", "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
+                + inputText.val())
+            imageBox.addClass("show-img")
+        } else {
+            // Adding class for error shaking animation.
+            inputText.addClass('error')
+            inputText.attr("style","border: 2px solid darkred;")
+            setTimeout(() => {
+                inputText.removeClass('error')
+                inputText.attr("style","")
+            }, 500)
         }
     }
 });
